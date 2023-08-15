63882564938353c73294287bc236f66136f3f6a9
Adam <adam.blake@au.janus.care>
2023-08-15 15:35:56 +1000

    Inital commit

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -8,10 +8,10 @@
           <ion-label>Tab 1</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
-        </ion-tab-button>
+		<ion-tab-button tab="tab2" href="/tabs/tab2">
+			<ion-icon :icon="images" />
+			<ion-label>Photos</ion-label>
+		</ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/tab3">
           <ion-icon aria-hidden="true" :icon="square" />
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { images, square, triangle } from 'ionicons/icons';
 </script>
